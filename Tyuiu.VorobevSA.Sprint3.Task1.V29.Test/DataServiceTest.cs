3a14f9b6e8dcb94f484ce7358b3476267a7c20a1
Tyuiu.VorobevSA@outlook.com <vorobewserezha2118@gmail.com>
2023-10-19 23:43:16 +0500

    Task1

---
+++ Tyuiu.VorobevSA.Sprint3.Task1.V29.Test/DataServiceTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.VorobevSA.Sprint3.Task1.V29.Lib;
+
+namespace Tyuiu.VorobevSA.Sprint3.Task1.V29.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetMultiplySeries()
+        {
+            DataService service1 = new DataService();
+
+            double value = 0.25;
+            int startValue = 1;
+            int stopValue = 11;
+
+            double res = service1.GetMultiplySeries(value, startValue, stopValue);
+
+            double wait = 3456091836.053;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.VorobevSA.Sprint3.Task1.V29.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.VorobevSA.Sprint3.Task1.V29.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.VorobevSA.Sprint3.Task1.V29.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4d955012-f9f3-4cb9-9690-69d75f54e875")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

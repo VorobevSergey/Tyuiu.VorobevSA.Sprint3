d0f8fd05cd8b79b289c8eb0469166477c58e8ffe
Tyuiu.VorobevSA@outlook.com <vorobewserezha2118@gmail.com>
2023-10-24 15:17:17 +0500

    Task5

---
+++ Tyuiu.VorobevSA.Sprint3.Task5.V11.Test/DataServiceTest.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.VorobevSA.Sprint3.Task5.V11.Lib;
+
+namespace Tyuiu.VorobevSA.Sprint3.Task5.V11.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetSumSumSeries()
+        {
+            DataService service1 = new DataService();
+
+            int x = 5;
+            int startValue1 = 1;
+            int stopValue1 = 3;
+            int startvalue2 = 1;
+            int stopvalue2 = 10;
+
+            double res = service1.GetSumSumSeries(x, startValue1, startvalue2, stopValue1, stopvalue2);
+
+            double wait = 64.234;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
--- Tyuiu.VorobevSA.Sprint3.Task5.V11/Program.cs
+++ Tyuiu.VorobevSA.Sprint3.Task5.V11/Program.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Tyuiu.VorobevSA.Sprint3.Task4.V16.Lib;
+using Tyuiu.VorobevSA.Sprint3.Task5.V11.Lib;
 
-namespace Tyuiu.VorobevSA.Sprint3.Task4.V16
+namespace Tyuiu.VorobevSA.Sprint3.Task5.V11
 {
     class Program
     {
@@ -16,29 +16,33 @@ namespace Tyuiu.VorobevSA.Sprint3.Task4.V16
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* Спринт #3                                                               *");
             Console.WriteLine("* Тема:Оператор цикла continue/break                                      *");
-            Console.WriteLine("* Задание #3.4                                                            *");
-            Console.WriteLine("* Вариант #16                                                             *");
+            Console.WriteLine("* Задание #3.5                                                            *");
+            Console.WriteLine("* Вариант #11                                                             *");
             Console.WriteLine("* Выполнил:Воробьев Сергей Александрович | ПКТб-23-2                      *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* УСЛОВИЕ:                                                                *");
-            Console.WriteLine("*На отрезке, где x принимает значения от -5 до 5, вычислить значение      *");
-            Console.WriteLine("*функции y=((cos(x)+x)/x)+0.25 При х = 0 пропустить значение Полученные   *");
-            Console.WriteLine("*значения перемножать.                                                    *");
+            Console.WriteLine("* вычислить значение выражения при х=5                                    *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
             Console.WriteLine("***************************************************************************");
 
-            int startValue = -5;
-            int stopValue = 5;
+            int x = 5;
+            int startValue1 = 1;
+            int stopValue1 = 3;
+            int startValue2 = 1;
+            int stopValue2 = 10;
 
-            Console.WriteLine("Старт шага: " + startValue);
-            Console.WriteLine("Конец шага: " + stopValue);
+            Console.WriteLine("х=5");
+            Console.WriteLine("Старт шага1: " + startValue1);
+            Console.WriteLine("Конец шага1: " + stopValue1);
+            Console.WriteLine("Старт шага2: " + startValue2);
+            Console.WriteLine("Конец шага2: " + stopValue2);
 
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
             Console.WriteLine("***************************************************************************");
 
-            Console.WriteLine("получилось=" + service1.Calculate(startValue,stopValue));
+            Console.WriteLine("получилось=" + service1.GetSumSumSeries(x, startValue1, startValue2, stopValue1, stopValue2));
             Console.ReadKey();
         }
     }
